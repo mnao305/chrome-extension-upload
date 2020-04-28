74f218ca6f0fefbc85cfd451b287e0df527627a7
mnao305 <naomasa305@gmail.com>
2020-04-28 23:32:49 +0900

    入力の読み込み

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    // TODO
+    const filePath = core.getInput('file-path', {required: true})
+    const extensionId = core.getInput('extension-id', {required: true})
+    const clientId = core.getInput('client-id', {required: true})
+    const clientSecret = core.getInput('client-secret', {required: true})
+    const refreshToken = core.getInput('refresh-token', {required: true})
+    const publishTarget = core.getInput('publish-target', {required: true})
+    const glob = core.getInput('glob')
   } catch (error) {
     core.setFailed(error.message)
   }
